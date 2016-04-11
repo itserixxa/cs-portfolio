3e055db005a855947ad597aee41b1904a10e3f10
itserixxa <ecolomer.sh@mysummitps.org>
2016-04-11 03:08:53 +0000

    first commit

--- plantgrowth.js
+++ plantgrowth.js
@@ -17,24 +17,10 @@ nextButton.addEventListener("click", function(){
     //this will line up the images in order when clicking them
     mainpicture.src = images[imagesIndex]
 });
-spring.addEventListener("click", function(){
-    info1.innerHTML = ""
-    info2.innerHTML = ""
-    info3.innerHTML = ""
+
+mainpicture.addEventListener("click", function(){
+    info1.innerHTML = "Seasons affect trees greatly, mainly with their leaves. In this first picture, you will see four different looking parts of the tree that are affected by each season. The far end of the tree is Spring,then after is Summer followed by Fall/Autumn, and Winter. In the next picture, Spring starts to occur aorund the beginning of the year while Winter is still ending. Animals return and begin to need plants to feed on or pollinate and trees start to grow again with new leaves and also begin to grow taller and reproduce to create more trees. Although the trees don't have many leaves, they can still give off oxygen anyways. "
+    info2.innerHTML = "In the third image, we see how trees turn out during Summer. Now trees are receiving a ton of energy and are fully grown with a lot of leaves which means that they are nice and healthy. "
+    info3.innerHTML = "In the last image, we now see shows how a tree would look during Fall/Autumn. This is when the leaves start to shrivel up and turn from a darkish green to colors like orange/brown, or sometimes yellow. This happens so the branches and leaves prepare for 'hibernation' because it will become very cold and the trees need to preserve all their energy for the next season which is Winter. In the last picture, you will see how Winter affects trees. Trees will usually be bare when it is really cold and even when it snows. Right after Winter the Cycle will continue again starting with Spring and so on. Seasons are caused by the rotations and titlting of the Earth's axis and we can see how it can affect living things, like trees "
     
-});
-summer.addEventListener("click", function(){
-    info1.innerHTML = ""
-    info2.innerHTML = ""
-    info3.innerHTML = ""
-});
-fall.addEventListener("click", function(){
-    info1.innerHTML = ""
-    info2.innerHTML = ""
-    info3.innerHTML = ""
-});
-winter.addEventListener("click",function(){
-   info1.innerHTML = ""
-   info2.innerHTML = ""
-   info3.innerHTML = ""
 });
